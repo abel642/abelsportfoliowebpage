af63a2b2fadd253805394c8d7b22d053fb5aeae5
abel642 <olamideakinbumiti@gmail.com>
2026-05-16 15:19:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function codinginfo (){
+	alert("CODING-coding is a a way developers give thier websites function HTML-A type of language used to make a webpage     CSS-The type of language used to design a webpage to make it look profetional and more detailed javaScript-The type of language used to make a webpage interactive in anyway possible!");
+}
+function MOTIVATION(){
+	alert("If you are good at coding,you can design anything!");
+	}
